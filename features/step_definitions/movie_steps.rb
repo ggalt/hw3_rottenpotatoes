9c7d7c33d013a6b26b7ad7b7fff6530da7b090a6
George Galt <george@galtfamily.net>
2012-10-24 02:51:47 +0000

    add movies problem

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,6 +2,9 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+    if !@movie.find(movie["title"])
+      @movie.create!(movie)
+    end
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
